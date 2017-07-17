c7f859d342898cf7421b388650389a48a8ee4fee
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-16 23:04:47 -0700

    Adding insertProduct helper method to insert some data into DB

--- app/src/main/java/com/example/android/inventoryapp/ProductListActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductListActivity.java
@@ -1,15 +1,21 @@
 package com.example.android.inventoryapp;
 
 import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+
+import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
 
 public class ProductListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
-
 
     /**
      * Identifier for the product data loader
@@ -26,7 +32,7 @@ public class ProductListActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
-        // Find the ListView which will be populated with the pet data
+        // Find the ListView which will be populated with the product data
         ListView productListView = (ListView) findViewById(R.id.list);
 
         // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
@@ -42,18 +48,75 @@ public class ProductListActivity extends AppCompatActivity implements
 
         // Kick off the loader
         getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
-
     }
 
+    /**
+     * Helper method to insert hardcoded product data into the database. For debugging purposes only.
+     */
+    private void insertProduct() {
+        // Create a ContentValues object where column names are the keys,
+        // and 'hard drive' attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, "hard drive");
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, "150.00");
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, "2");
+        values.put(ProductEntry.COLUMN_PRODUCT_IMAGERESOURCEID, "-1");
+
+        // Insert a new row for 'hard drive' into the provider using the ContentResolver.
+        // Use the {@link ProductEntry#CONTENT_URI} to indicate that we want to insert
+        // into the products database table.
+        // Receive the new content URI that will allow us to access 'hard drive' data in the future.
+        Uri newUri = getContentResolver().insert(ProductEntry.CONTENT_URI, values);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_productlist.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_productlist, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertProduct();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     @Override public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductEntry.COLUMN_PRODUCT_IMAGERESOURCEID};
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                ProductEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
     }
 
     @Override public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-
+        // Update {@link ProductCursorAdapter} with this new cursor containing updated product data
+        mCursorAdapter.swapCursor(cursor);
     }
 
     @Override public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
 
     }
 }
--- app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -1,13 +1,63 @@
 package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class ProductProvider extends ContentProvider {
+
+    /**
+     * Tag for the log messages
+     */
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
+
+    /**
+     * URI matcher code for the content URI for the products table
+     */
+    private static final int PRODUCTS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single product in the products table
+     */
+    private static final int PRODUCTS_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        // The content URI of the form "content://com.example.android.inventoryapp/products"
+        // will map to the integer code {@link #PRODUCTS}.
+        //  This URI is used to provide access to MULTIPLE rows
+        // of the products table.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS, PRODUCTS);
+
+        // The content URI of the form "content://com.example.android.inventoryapp/products/#"
+        // will map to the integer code {@link #PRODUCTS_ID}.
+        //  This URI is used to provide access to ONE single row
+        // of the products table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.example.android.inventoryapp/products/3" matches, but
+        // "content://com.example.android.inventoryapp/products"
+        // (without a number at the end) doesn't match.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS + "/#", PRODUCTS_ID);
+    }
 
     /**
      * Database helper object
@@ -19,16 +69,45 @@ public class ProductProvider extends ContentProvider {
         return true;
     }
 
+    @Nullable @Override public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return insertProduct(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertProduct(Uri uri, ContentValues contentValues) {
+
+        // Check that the name is not null
+        String name = contentValues.getAsString(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
+
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new product with the given values
+        long id = database.insert(ProductContract.ProductEntry.TABLE_NAME, null, contentValues);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        // Notify all listeners that the data has changed for the product content URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return the new URI with the ID (of the newly inserted row) appended at the end
+        return ContentUris.withAppendedId(uri, id);
+    }
+
     @Nullable @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
-    }
-
-    @Nullable @Override public String getType(@NonNull Uri uri) {
-        return null;
-    }
-
-    @Nullable @Override public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
         return null;
     }
 
@@ -40,4 +119,18 @@ public class ProductProvider extends ContentProvider {
     public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
         return 0;
     }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return ProductContract.ProductEntry.CONTENT_LIST_TYPE;
+            case PRODUCTS_ID:
+                return ProductContract.ProductEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
+    }
+
 }
