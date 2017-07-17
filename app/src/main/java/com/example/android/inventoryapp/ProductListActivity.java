297d46f6f5103ec547d09957717d3aa96a496c17
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-16 21:15:25 -0700

    Added ListView, ProductListActivity to the manifest file incl. the provider

--- app/src/main/java/com/example/android/inventoryapp/ProductListActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductListActivity.java
@@ -1,13 +1,59 @@
 package com.example.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-public class ProductListActivity extends AppCompatActivity {
+public class ProductListActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+
+    /**
+     * Identifier for the product data loader
+     */
+    private static final int PRODUCT_LOADER = 0;
+
+    /**
+     * Adapter for the ListView
+     */
+    ProductCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        // Find the ListView which will be populated with the pet data
+        ListView productListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+     /*   View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);*/
+
+        // Setup an Adapter to create a list item for each row of product data in the Cursor.
+        // There is no product data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        // TODO: Setup the item click listener
+
+        // Kick off the loader
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+
+    }
+
+    @Override public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
