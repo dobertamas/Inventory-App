1c3963fe4f16c1ffc824bb0a84e7344ddafbed58
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-23 09:27:21 -0700

    Adding AlertDialog; delete product is not a button but an option

--- app/src/main/java/com/example/android/inventoryapp/ProductDetailActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductDetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.inventoryapp;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
@@ -99,6 +101,11 @@ public class ProductDetailActivity extends AppCompatActivity implements
                 saveProduct();
                 // Exit activity
                 finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete_product:
+                // Pop up confirmation dialog for deletion
+                showDeleteConfirmationDialog();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -266,8 +273,37 @@ public class ProductDetailActivity extends AppCompatActivity implements
 
     }
 
-    @OnClick(R.id.product_detail_quantity_delete_product)
+    /**
+     * Prompt the user to confirm that they want to delete this product.
+     */
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Delete this product?");
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the product.
+                deleteProduct();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the product.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
     public void deleteProduct() {
+
         Uri currentProductUri = ContentUris.withAppendedId(ProductContract.ProductEntry.CONTENT_URI, mProductId);
         Log.i(LOG_TAG, " currentProductUri: " + currentProductUri.toString());
 
